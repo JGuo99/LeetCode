735dc607fb68ae1a097ca5cf632362134bb2cebd
JGuo99 <jguo0310@gmail.com>
2023-01-19 21:53:10 -0500

    Added Dijkstra Algo

--- Templates/GraphTemplate/Dijkstra.java
+++ Templates/GraphTemplate/Dijkstra.java
@@ -2,78 +2,65 @@ package GraphTemplate;
 import java.util.*;
 
 public class Dijkstra {
-    static final int INF = Integer.MAX_VALUE;
-    static ArrayList<ArrayList<Node>> adjList = new ArrayList<>();
-    static int[] dist;
-    static boolean[] visited;
-    static int V, E;
-    static int start;
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-//        V = sc.nextInt();
-//        E = sc.nextInt();
-        V = 5;
-        E = 4;
-        dist = new int[V];
-        visited = new boolean[V];
-
-        for (int i = 0; i < V; i++) {
-            adjList.add(new ArrayList<>());
-            dist[i] = INF;
-        }
-
-        for (int i = 0; i < E; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
-            adjList.get(u).add(new Node(v, w));
-            adjList.get(v).add(new Node(u, w));
-        }
-
-        start = sc.nextInt();
-        dijkstra(start);
-
-        for (int i = 0; i < V; i++) {
-            System.out.println(i + " " + dist[i]);
-        }
+        int[][] edges = new int[7][3];
+        // a:0, b:1, c:2, d:3, e:4
+        edges[0] = new int[] {0, 1, 10};
+        edges[1] = new int[] {0, 2, 3};
+        edges[2] = new int[] {1, 3, 2};
+        edges[3] = new int[] {2, 1, 4};
+        edges[4] = new int[] {2, 3, 8};
+        edges[5] = new int[] {2, 4, 2};
+        edges[6] = new int[] {3, 4, 5};
+        System.out.println(dijkstra(edges, 6, 0));
     }
 
-    public static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
+    /*
+        Complexity
+            - Time: O(E*logV) - From the MinHeap
+            - Space: O(V)
+        MinHeap (PriorityQueue): <Node>
+        Node: <Vertex, Cost>
+            - Cost: to reach the Vertex
+            - Sort by Cost
+        @param edges: 2D Array of edges where...
+            - edge[0] = src    (Starting Node)
+            - edge[1] = dest   (Ending Node)
+            - edge[2] = weight (Weight of the Edge)
+        @param n: Number of Nodes (Assuming the Nodes are Integers)
+        @param start: starting Node
+        @return shortestPath: (Node, TotalWeight)
+     */
+    public static Map<Integer, Integer> dijkstra(int[][] edges, int n, int start) {
+        // Set-Up
+        // Create the Map of Nodes
+        Map<Integer, List<int[]>> neighbors = new HashMap<>();
+        for (int i = 0; i < n; i++) neighbors.computeIfAbsent(i, k -> new ArrayList<>());
+        // Add the edges and weight to the Nodes: 0: Src. Node, 1: Dest. Node, 2: Weight
+        for (int[] edge : edges) neighbors.get(edge[0]).add(new int[]{edge[1], edge[2]});
+
+        Map<Integer, Integer> shortestPath = new HashMap<>();
+        // (Current Node, Weight Cost)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> (a[1] - b[1])
+        );
+        pq.add(new int[]{start, 0});
 
         while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-            int curDist = cur.dist;
-            int curNode = cur.node;
+            int[] cur = pq.remove();
+            int node1 = cur[0];
+            int weight1 = cur[1];
 
-            if (visited[curNode]) {
-                continue;
-            }
-            visited[curNode] = true;
-
-            for (Node n : adjList.get(curNode)) {
-                if (dist[n.node] > curDist + n.dist) {
-                    dist[n.node] = curDist + n.dist;
-                    pq.offer(new Node(n.node, dist[n.node]));
+            if (shortestPath.containsKey(node1)) continue;
+            shortestPath.put(node1, weight1);
+            for (int[] neighbor : neighbors.get(node1)) {
+                int node2 = neighbor[0];
+                int weight2 = neighbor[1];
+                if (!shortestPath.containsKey(node2)) {
+                    pq.add(new int[]{node2, weight1 + weight2});
                 }
             }
         }
-    }
-}
-
-class Node implements Comparable<Node> {
-    int node;
-    int dist;
-
-    public Node(int node, int dist) {
-        this.node = node;
-        this.dist = dist;
-    }
-
-    public int compareTo(Node n) {
-        return dist - n.dist;
+        return shortestPath;
     }
 }
